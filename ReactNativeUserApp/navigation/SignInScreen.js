a2d4e1380825eb95b8fa73373067d8b0dbd48005
tranminhlong0000 <tranminhlong0000@gmail.com>
2019-10-04 21:46:31 +0700

    DevC challenge: Final Commit version 1

--- ReactNativeUserApp/navigation/SignInScreen.js
+++ ReactNativeUserApp/navigation/SignInScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import {
   View,
   Button,
@@ -7,94 +7,92 @@ import {
   ActivityIndicator,
   TouchableWithoutFeedback,
   TouchableOpacity,
-  StyleSheet
-} from "react-native";
-import * as IntentLauncher from "expo-intent-launcher";
-import * as Location from "expo-location";
-import * as Permissions from "expo-permissions";
-import { connect } from "react-redux";
-import { Entypo } from "@expo/vector-icons";
-import { Input } from "react-native-elements";
+  StyleSheet,
+  ToastAndroid,
+} from 'react-native';
+import * as IntentLauncher from 'expo-intent-launcher';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import {connect} from 'react-redux';
+import {Entypo} from '@expo/vector-icons';
+import {Input, SocialIcon} from 'react-native-elements';
 import {
   getCurrentLocation,
   userDenyGetCurrentLocation,
   waitForGetLocation,
   changeMapRange,
   initUserId,
-  getUserId
-} from "../redux/UserRedux";
-import { getCategories } from "../redux/CategoryRedux";
+  getUserId,
+  facebookLogin,
+} from '../redux/UserRedux';
+import {getCategories} from '../redux/CategoryRedux';
 
 class SignInScreen extends Component {
   static navigationOptions = {
-    header: null
+    header: null,
   };
 
   state = {
-    id: "",
-    email: "",
-    test: false // normal: false, testing: true
+    id: '',
+    email: '',
+    test: false, // normal: false, testing: true
   };
-  componentDidMount() {
-    this.props.getCategories();
+  componentDidMount () {
+    this.props.getCategories ();
   }
 
   _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== "granted") {
+    let {status} = await Permissions.askAsync (Permissions.LOCATION);
+    if (status !== 'granted') {
       //redux save
-      console.log("Permission to access location was denied");
+      console.log ('Permission to access location was denied');
     }
     try {
-      const location = await Location.getCurrentPositionAsync({});
-      this.props.getUserLocation({
+      const location = await Location.getCurrentPositionAsync ({});
+      this.props.getUserLocation ({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
         latitudeDelta: 0.0022727,
-        longitudeDelta: 0.0022727
+        longitudeDelta: 0.0022727,
       });
-      this.props.initMapRange(0.0022727); //250m
+      this.props.initMapRange (0.0022727); //250m
     } catch (err) {
-      console.log(err);
-      this.props.getUserLocationFail();
+      console.log (err);
+      this.props.getUserLocationFail ();
     }
   };
 
   onChangeInput = e => {
-    this.setState({ email: e });
+    this.setState ({email: e});
   };
   onChangeIdInputTest = e => {
-    this.setState({ id: e });
+    this.setState ({id: e});
   };
 
   onClick = async () => {
     try {
-      const locationAccepted = await IntentLauncher.startActivityAsync(
+      const locationAccepted = await IntentLauncher.startActivityAsync (
         IntentLauncher.ACTION_LOCATION_SOURCE_SETTINGS
       );
       if (locationAccepted) {
-        this.props.isLoading();
-        await this._getLocationAsync();
+        this.props.isLoading ();
+        await this._getLocationAsync ();
       }
     } catch (err) {}
   };
 
-  render() {
-    const { user } = this.props;
+  render () {
+    const {user} = this.props;
     if (user.isLoading) {
       return (
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <ActivityIndicator size="large" color="blue" />
         </View>
       );
     }
     if (!user.hasLocation) {
       return (
-        <View
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Text>Hãy cung cấp vị trí của bạn để truy cập ứng dụng.</Text>
           <Button title="GOT IT!" onPress={this.onClick} />
         </View>
@@ -102,108 +100,131 @@ class SignInScreen extends Component {
     }
     return (
       <TouchableWithoutFeedback
-        style={{ flex: 1 }}
+        style={{flex: 1}}
         delayLongPress={5000}
         onLongPress={this.changeAppStatus}
       >
-        <View
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
-          {!this.state.test ? (
-            <React.Fragment>
-              <Input
-                value={this.state.email}
-                onChangeText={this.onChangeInput}
-                placeholder="Nhập email của bạn nào."
-                leftIconContainerStyle={{ marginRight: 10 }}
-                leftIcon={<Entypo name="mail" size={24} color="black" />}
-              />
-              <TouchableOpacity
-                onPress={this.checkUserExist}
-                style={styles.button}
-              >
-                <Text style={styles.buttonText}>Đăng nhập</Text>
-              </TouchableOpacity>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <Input
-                value={this.state.id}
-                onChangeText={this.onChangeIdInputTest}
-                placeholder="Nhập userId để test"
-                leftIcon={<Entypo name="mail" size={24} color="black" />}
-                leftIconContainerStyle={{ marginRight: 10 }}
-              />
-              <TouchableOpacity
-                onPress={this.testMethod}
-                style={styles.button}
-              >
-                <Text style={styles.buttonText}>Test thôi!</Text>
-              </TouchableOpacity>
-            </React.Fragment>
-          )}
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          {!this.state.test
+            ? <React.Fragment>
+                <Input
+                  value={this.state.email}
+                  onChangeText={this.onChangeInput}
+                  placeholder="Nhập email của bạn nào."
+                  leftIconContainerStyle={{marginRight: 10}}
+                  leftIcon={<Entypo name="mail" size={24} color="black" />}
+                />
+                <TouchableOpacity
+                  onPress={this.checkUserExist}
+                  style={styles.button}
+                >
+                  <Text style={styles.buttonText}>Đăng nhập</Text>
+                </TouchableOpacity>
+                <SocialIcon
+                  style={styles.button}
+                  title="Đăng nhập với facebook"
+                  button
+                  type="facebook"
+                  onPress={this.fbLogIn}
+                />
+              </React.Fragment>
+            : <React.Fragment>
+                <Input
+                  value={this.state.id}
+                  onChangeText={this.onChangeIdInputTest}
+                  placeholder="Nhập userId để test"
+                  leftIcon={<Entypo name="mail" size={24} color="black" />}
+                  leftIconContainerStyle={{marginRight: 10}}
+                />
+                <TouchableOpacity
+                  onPress={this.testMethod}
+                  style={styles.button}
+                >
+                  <Text style={styles.buttonText}>Test thôi!</Text>
+                </TouchableOpacity>
+              </React.Fragment>}
         </View>
       </TouchableWithoutFeedback>
     );
   }
 
   checkUserExist = () => {
-    getUserId(this.state.email).then(res => {
-      this.props.initUserId(res.data.id);
-      this._signInAsync(res.data.id);
-    });
+    getUserId (this.state.email)
+      .then (res => {
+        this.props.initUserId (res.data.id);
+        this._signInAsync (res.data.id);
+      })
+      .catch (err => {
+        //handle new user after
+        this.props.initUserId ('0');
+        this._signInAsync ('0');
+      });
   };
 
-  _signInAsync = async (token) => {
+  _signInAsync = async token => {
     // gán userID sau
-    await AsyncStorage.setItem("userToken", JSON.stringify(token));
-    this.props.navigation.navigate("Main");
+    await AsyncStorage.setItem ('userToken', JSON.stringify (token));
+    this.props.navigation.navigate ('Main');
   };
 
   testMethod = () => {
-    this.props.initUserId(this.state.id)
-    this._signInAsync(this.state.id);
-  }
+    this.props.initUserId (this.state.id);
+    this._signInAsync (this.state.id);
+  };
 
   changeAppStatus = () => {
-    this.setState({ test: !this.state.test });
+    this.setState ({test: !this.state.test});
+  };
+
+  fbLogIn = async () => {
+    const userEmail = await facebookLogin ();
+    ToastAndroid.show ('Đợi tí nha. Tui đang xử lý', ToastAndroid.BOTTOM);
+    console.log (userEmail);
+    getUserId (userEmail)
+      .then (res => {
+        this.props.initUserId (res.data.id);
+        this._signInAsync (res.data.id);
+      })
+      .catch (err => {
+        console.log ('người ngoài');
+        //handle new user after
+        this.props.initUserId ('0');
+        this._signInAsync ('0');
+      });
   };
 }
 
 const mapStateToProps = state => {
   return {
-    user: state.user
+    user: state.user,
   };
 };
 const mapDispatchToProps = dispatch => {
   return {
-    getUserLocation: region => dispatch(getCurrentLocation(region)),
-    getUserLocationFail: () => dispatch(userDenyGetCurrentLocation()),
-    isLoading: () => dispatch(waitForGetLocation()),
-    getCategories: () => dispatch(getCategories()),
-    initMapRange: range => dispatch(changeMapRange(range)),
-    initUserId: userId => dispatch(initUserId(userId))
+    getUserLocation: region => dispatch (getCurrentLocation (region)),
+    getUserLocationFail: () => dispatch (userDenyGetCurrentLocation ()),
+    isLoading: () => dispatch (waitForGetLocation ()),
+    getCategories: () => dispatch (getCategories ()),
+    initMapRange: range => dispatch (changeMapRange (range)),
+    initUserId: userId => dispatch (initUserId (userId)),
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SignInScreen);
+export default connect (mapStateToProps, mapDispatchToProps) (SignInScreen);
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   button: {
-    borderColor: "#ddd",
+    borderColor: '#ddd',
     borderWidth: 1,
     padding: 10,
     marginTop: 10,
     borderRadius: 20,
-    width: 200,
-    justifyContent: "center",
-    alignItems: "center"
+    width: 270,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonText: {
     fontSize: 16,
-    fontWeight: "400"
-  }
+    fontWeight: '400',
+  },
 });
--- ReactNativeUserApp/redux/UserRedux.js
+++ ReactNativeUserApp/redux/UserRedux.js
@@ -1,95 +1,119 @@
-import { foodRecommedApi } from "../constants/AxiosConfig";
+import {Alert} from 'react-native';
+import * as Facebook from 'expo-facebook';
+import {foodRecommedApi} from '../constants/AxiosConfig';
 
 const types = {
-  GET_CURRENT_LOCATION: "GET_CURRENT_LOCATION",
-  GET_CURRENT_LOCATION_FAIL: "GET_CURRENT_LOCATION_FAIL",
-  LOADING_GET_LOCATION: "LOADING_GET_LOCATION",
-  ON_CHANGE_MAP_RANGE: "ON_CHANGE_MAP_RANGE",
-  ON_CHANGE_MAP_REGION: "ON_CHANGE_MAP_REGION",
-  INIT_USERID: "INIT_USERID"
+  GET_CURRENT_LOCATION: 'GET_CURRENT_LOCATION',
+  GET_CURRENT_LOCATION_FAIL: 'GET_CURRENT_LOCATION_FAIL',
+  LOADING_GET_LOCATION: 'LOADING_GET_LOCATION',
+  ON_CHANGE_MAP_RANGE: 'ON_CHANGE_MAP_RANGE',
+  ON_CHANGE_MAP_REGION: 'ON_CHANGE_MAP_REGION',
+  INIT_USERID: 'INIT_USERID',
 };
 
 export const getCurrentLocation = userRegion => dispatch => {
-  dispatch({
+  dispatch ({
     type: types.GET_CURRENT_LOCATION,
-    payload: userRegion
+    payload: userRegion,
   });
 };
 
 export const userDenyGetCurrentLocation = () => dispatch => {
-  dispatch({
+  dispatch ({
     type: types.GET_CURRENT_LOCATION_FAIL,
     payload: {
-      hasLocation: false
-    }
+      hasLocation: false,
+    },
   });
 };
 
 export const waitForGetLocation = () => dispatch => {
-  dispatch({
+  dispatch ({
     type: types.LOADING_GET_LOCATION,
     payload: {
-      isLoading: true
-    }
+      isLoading: true,
+    },
   });
 };
 
 export const changeMapRange = range => dispatch => {
-  dispatch({
+  dispatch ({
     type: types.ON_CHANGE_MAP_RANGE,
-    payload: range
+    payload: range,
   });
 };
 
 export const changeMapRegion = region => dispatch => {
-  dispatch({
+  dispatch ({
     type: types.ON_CHANGE_MAP_REGION,
-    payload: region
+    payload: region,
   });
 };
 
 export const initUserId = userId => dispatch => {
-  dispatch({
+  dispatch ({
     type: types.INIT_USERID,
-    payload: userId
+    payload: userId,
   });
 };
 
 export const getUserId = email => {
-  return foodRecommedApi
-    .post("/api/account/getuserid", {
-      email: email
-    })
+  return foodRecommedApi.post ('/api/account/getuserid', {
+    email: email,
+  });
+};
+
+export const facebookLogin = async () => {
+  try {
+    const {
+      type,
+      token,
+    } = await Facebook.logInWithReadPermissionsAsync ('2422614771315615', {
+      permissions: ['public_profile', 'email'],
+    });
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch (
+        `https://graph.facebook.com/me?fields=email&access_token=${token}`
+      );
+      const userInf = await response.json ()
+      return userInf.email;
+    } else {
+      Alert.alert ('Có lỗi khi đăng nhập với facebook.');
+    }
+  } catch ({message}) {
+    alert (`Có lỗi khi đăng nhập với facebook: ${message}`);
+  }
 };
 
 const initState = {
   hasLocation: false,
   isLoading: true,
   region: {},
-  userId: null
+  userId: null,
 };
 
 export const reducer = (state = initState, action) => {
-  const { payload, type } = action;
+  const {payload, type} = action;
   switch (type) {
     case types.GET_CURRENT_LOCATION:
       return {
         ...state,
         region: payload,
         isLoading: false,
-        hasLocation: true
+        hasLocation: true,
       };
     case types.GET_CURRENT_LOCATION_FAIL:
       return {
         ...state,
         region: {},
         hasLocation: false,
-        isLoading: false
+        isLoading: false,
       };
     case types.LOADING_GET_LOCATION:
       return {
         ...state,
-        isLoading: true
+        isLoading: true,
       };
     case types.ON_CHANGE_MAP_RANGE:
       return {
@@ -97,8 +121,8 @@ export const reducer = (state = initState, action) => {
         region: {
           ...state.region,
           latitudeDelta: payload,
-          longitudeDelta: payload
-        }
+          longitudeDelta: payload,
+        },
       };
     case types.ON_CHANGE_MAP_REGION:
       return {
@@ -106,13 +130,13 @@ export const reducer = (state = initState, action) => {
         region: {
           ...state.region,
           latitude: payload.latitude,
-          longitude: payload.longitude
-        }
+          longitude: payload.longitude,
+        },
       };
     case types.INIT_USERID:
       return {
         ...state,
-        userId: payload
+        userId: payload,
       };
     default:
       return state;
